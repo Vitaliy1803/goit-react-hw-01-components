86d705411a9948efc25273aab6f8ea0e784ba261
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-09-01 00:22:49 +0300

    fix title &&

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,36 +1,37 @@
 import PropTypes from 'prop-types';
-import { Stats,
-        Title,
-        StatList,
-        Item,
-        Label,
-        Percentage
+import {
+  Stats,
+  Title,
+  StatList,
+  Item,
+  Label,
+  Percentage,
 } from './Statistics.styled';
 
-export const Statistics = ({title, stats}) => {
-    return (
-        <Stats>
-        <Title>{title}</Title>
+export const Statistics = ({ title, stats }) => {
+  return (
+    <Stats>
+      {title && <Title>{title}</Title>}
 
-  <StatList>
-    {stats.map(stat => (
-        <Item key={stat.id}>
+      <StatList>
+        {stats.map(stat => (
+          <Item key={stat.id}>
             <Label>{stat.label}</Label>
             <Percentage>{stat.percentage}%</Percentage>
-        </Item>
-    ))}
-  </StatList>
-</Stats>
-    );
+          </Item>
+        ))}
+      </StatList>
+    </Stats>
+  );
 };
 
 Statistics.propTypes = {
-    title: PropTypes.string,
-    stats: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        label: PropTypes.string.isRequired,
-        percentage: PropTypes.number.isRequired,
-      })
-    ).isRequired,
-  };
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
